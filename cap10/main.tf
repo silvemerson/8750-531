26517b27edc5a8a615caa9acc54022327d469e1f
silvemerson <araujo.emerson28@gmail.com>
2025-05-26 11:10:05 -0300

    definido criacao de instance no main raiz

--- cap10/main.tf
+++ cap10/main.tf
@@ -18,7 +18,7 @@ module "subnetwork" {
 
 module "ip_externo" {
     source = "./gcp-public-ip"
-    name = "ip-externo-modulo"
+    name = var.ip_publico
   
 }
 
@@ -38,4 +38,21 @@ module "firewall" {
     network = var.vpc_name
     source_ranges = var.source_ranges
     depends_on = [ module.network, module.subnetwork ]
+}
+
+
+module "vm-instance" {
+    source = "./gcp-instance"
+    name = var.vm_name
+    machine_type = var.machine_type
+    zone = var.zone
+    allow_stopping_for_update = var.allow_stopping_for_update
+    image = var.image
+    ssh-keys = "tux:${file("/home/emerson/4Linux/8740-524-ssh-keys/8740-524.pub")}"
+
+    network = var.vpc_name
+    subnetwork = "subnet-modulo"
+    nat_ip = module.ip_externo.ip_externo
+
+    depends_on = [ module.ip_externo ]
 }
