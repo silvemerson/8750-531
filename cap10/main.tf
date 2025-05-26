8dbfb5c9b8cea76b4f9b869dbc55bbf674f3f798
silvemerson <araujo.emerson28@gmail.com>
2025-05-26 10:21:50 -0300

    update main com os novos modulos

--- cap10/main.tf
+++ cap10/main.tf
@@ -1,7 +1,7 @@
 module "network" {
     source = "./gcp-network"
 
-    name = "tf-chapter10"
+    name = var.vpc_name
     description = "Rede do capitulo 10"
   
 }
@@ -11,7 +11,8 @@ module "subnetwork" {
     
     name = "subnet-modulo"
     ip_cidr_range = "192.168.10.0/24"
-    network = "tf-chapter10"
+    network = var.vpc_name
+    depends_on = [ module.network ]
   
 }
 
@@ -19,4 +20,22 @@ module "ip_externo" {
     source = "./gcp-public-ip"
     name = "ip-externo-modulo"
   
+}
+
+module "disk" {
+    source = "./gcp-disk"
+    name = "disco-modulo"
+    type = "pd-ssd"
+    size = 20
+    description = "teste"
+    zone = var.zone
+  
+}
+
+module "firewall" {
+    source = "./gcp-firewall"
+    name = var.fw_name
+    network = var.vpc_name
+    source_ranges = var.source_ranges
+    depends_on = [ module.network, module.subnetwork ]
 }
